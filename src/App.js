a3b277ae5283bc89dc210ca6d3d77fdd46b0e38c
sameer <sameerpokharel29@gmail.com>
2021-08-12 11:34:46 +0545

    added setup and global styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
-
+import {GlobalStyle} from './GlobalStyle'
+import { Normalize } from 'styled-normalize';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 function App() {
   return (
     <div>
-      App Placeholder
+      <Router>
+      <Normalize/>
+      <GlobalStyle/>
+      <Switch>
+          <Route path="/search">Search</Route>
+          <Route path="/">Home</Route>
+        </Switch>
+       </Router>
     </div>
   );
 }
